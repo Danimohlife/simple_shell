b0feaef9282cf6a6cdcb3fabd8490d95231f8ae0
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-16 12:59:57 -0700

    len

--- helperfunction.c
+++ helperfunction.c
@@ -1,10 +1,27 @@
 #include "main.h"
 /**
  * _putchar - alx version of stdio lib func putchar
- * @c: one char arg to print at a time
+ * @b: one char arg to print at a time
  * Return: c
  */
 int _putchar(char b)
 {
 	return (write(1, &b, 1));
+}
+
+/**
+ * _strlen - len of string
+ * @filename: file
+ * Return: len
+ */
+int _strlen(char *filename)
+{
+	int len = 0;
+
+	while (*filename != '\0')
+	{
+		filename++;
+		len++;
+	}
+	return (len);
 }
