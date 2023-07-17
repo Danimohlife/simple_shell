6a94c40bb2667b3422653e108fddf8e24b094ac5
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-17 09:17:37 -0700

    strchr

---
+++ strfunc.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strchr - char
+ * @str: string
+ * @ch: char
+ * Return: chara
+ */
+char *_strchr(char *str, char ch)
+{
+	do {
+		if (*str == c)
+			break;
+	} while (*str++);
+
+	return (str);
+}
