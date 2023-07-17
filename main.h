09bb2d2ef59c10bff1eea19fda7a60a1d203ce87
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-17 09:19:58 -0700

    h

--- main.h
+++ main.h
@@ -3,6 +3,7 @@ void _puts(char *filename);
 int _putchar(char c);
 int _atoi(char *str);
 char *_strcat(char *destfile, char *srcfile);
+char *_strchr(char *str, char ch);
 
 /* DANIMOH STRING FUNCTION */
 int _strlen(char *filename);
