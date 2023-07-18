8db3efb4afcd1db568f0e7edee094627b5f1ba2e
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-18 01:02:32 -0700

    strcmp

--- main.h
+++ main.h
@@ -1,9 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+
 /* Chinenye Normal run fubction */
 void _puts(char *filename);
 int _putchar(char c);
 int _atoi(char *str);
 char *_strcat(char *destfile, char *srcfile);
 char *_strchr(char *str, char ch);
+char _strcmp(char *s1, char *s2);
 
 /* DANIMOH STRING FUNCTION */
 int _strlen(char *filename);
--- strfunc.c
+++ strfunc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strchr - char
  * @str: string
@@ -8,9 +9,37 @@
 char *_strchr(char *str, char ch)
 {
 	do {
-		if (*str == c)
+		if (*str == ch)
 			break;
 	} while (*str++);
 
 	return (str);
 }
+/**
+ * _strcmp - strings compared
+ * @s1: string to compare
+ * @s2: string to compare to
+ * Return: de copamr
+ */
+char _strcmp(char *s1, char *s2)
+{
+	int a, b = 0;
+
+	if ((_strlen(s1) != _strlen(s2)) || (s1 == NULL) || (s2 == NULL))
+		return (1);
+
+	for (a = 0; s1[a]; a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			b = s1[a] - s2[a];
+			break;
+		}
+		else
+		{
+			continue;
+		}
+
+	}
+	return (b);
+
