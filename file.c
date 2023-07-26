f119b19b9aa4436c08b3a672c7d1a5d77d101e83
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-26 01:33:06 +0100

    edit func cus of core dump

--- file.c
+++ file.c
@@ -6,23 +6,21 @@
  */
 int tm_fc(char *pts)
 {
-	char *str = ".shell_history";
-	ssize_t pf, pwrit;
+	char *name = ".simple_shell_history";
+	ssize_t fd, wr;
 	int lp = 0;
 
-	if (!str)
+	if (!name)
 		return (-1);
-
-	pf = open(str, O_CREAT | O_RDWR | O_APPEND, 00600);
-
-	if (pf < 0)
+	fd = open(name, O_CREAT | O_RDWR | O_APPEND, 00600);
+	if (fd < 0)
 		return (-1);
 	if (pts)
 	{
 		while (pts[lp])
 			lp++;
-		pwrit = write(pf, pts, lp);
-		if (pwrit < 0)
+		wr = write(fd, pts, lp);
+		if (wr < 0)
 			return (-1);
 	}
 	return (1);
@@ -35,16 +33,15 @@ int tm_fc(char *pts)
 int echo_com(char **command)
 {
 	pid_t pid;
-	int status;
+	int i;
 
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve("/bin/echo", command, environ) == -1)
-		{
-			return (-1);
-		}
-
+	if (execve("/bin/echo", command, environ) == -1)
+	{
+		return (-1);
+	}
 		exit(EXIT_FAILURE);
 	}
 	else if (pid < 0)
@@ -54,11 +51,12 @@ int echo_com(char **command)
 	else
 	{
 		do {
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+			waitpid(pid, &i, WUNTRACED);
+		} while (!WIFEXITED(i) && !WIFSIGNALED(i));
 	}
 	return (1);
 }
+
 /**
  * tm_func - diplay all that is done.
  * @str: not used
@@ -67,31 +65,31 @@ int echo_com(char **command)
  */
 int tm_func(__attribute__((unused))char **str, __attribute__((unused))int i)
 {
-	char *file = ".shell_history";
-	char *line = NULL;
-	size_t pln = 0;
+	char *name = ".simple_shell_history";
+	FILE *fp;
+	char *strl = NULL;
+	size_t n = 0;
 	int lp = 0;
-	char *ch;
-	FILE *ptr;
+	char *c;
 
-	ptr = fopen(file, "r");
-	if (ptr == NULL)
+	fp = fopen(name, "r");
+	if (fp == NULL)
 	{
 		return (-1);
 	}
-
-	while ((getline(&line, &pln, ptr)) != -1)
+	while ((getline(&strl, &n, fp)) != -1)
 	{
 		lp++;
-		ch = _itoa(lp);
-		PUTCHAR(ch);
-		free(ch);
+		c = _itoa(lp);
+		PUTCHAR(er);
+		free(c);
 		PUTCHAR(" ");
-		PUTCHAR(line);
+		PUTCHAR(strl);
+
 	}
-	if (line)
-		free(line);
-	fclose(ptr);
+	if (strl)
+		free(strl);
+	fclose(fp);
 	return (0);
 }
 /**
@@ -101,17 +99,16 @@ int tm_func(__attribute__((unused))char **str, __attribute__((unused))int i)
  * @cd:Command
  * Return: Void function
  */
-void _print_er(char **ac, int i, char **cd)
+void _print_er(char *cd, int i, char **ac)
 {
-	char *c = _itoa(i);
+	char *c;
 
 	PUTCHAR(ac[0]);
 	PUTCHAR(": ");
-	PUTCHAR(c);
+	c = _itoa(i);
+	PUTCHAR(er);
+	free(er);
 	PUTCHAR(": ");
-	PUTCHAR(cd[0]);
-	PUTCHAR(": Illegal number: ");
-	PUTCHAR(cd[1]);
-	PUTCHAR("\n");
-	free(c);
+	PUTCHAR(cd);
+	PUTCHAR(": not found\n");
 }
